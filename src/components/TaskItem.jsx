3e01ac5f771c76f096c95789c2d6ef2707edb37d
Lucas Gomes <lucaass1997rp@gmail.com>
2024-06-15 13:58:08 -0300

    feat: fetch tasks after deletion

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -2,16 +2,20 @@ import { AiFillDelete } from "react-icons/ai";
 import axios from 'axios';
 import { useAlert } from "react-alert"; 
 
-
 import "./TaskItem.scss";
 
 
-const TaskItem = ({ task }) => {
+const TaskItem = ({ task, fetchTasks }) => {
     const alert = useAlert();
 
     const handleTaskDeletion = async () => {
         try {
             await axios.delete(`http://localhost:8000/tasks/${task._id}`);
+
+            await fetchTasks()
+
+            alert.success("A tarefa foi removida com sucesso!")
+
         } catch (error) {
             alert.error("Algo deu errado.");
         }
